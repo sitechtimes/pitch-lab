967a5e528441c88e6ac7f22c18a9fd9174b6d013
Verviam <gaoevan37@gmail.com>
2024-12-02 10:17:10 -0500

    file organizing

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
   <div class="w-screen bg-[#2A283B]">
     <header class="flex justify-between">
       <h1 class="text-white text-5xl m-6">Pitch Lab</h1>
+      <!-- HeaderTitle -->
       <SettingsIcon />
     </header>
 
@@ -21,7 +22,8 @@
 </template>
 
 <script setup>
-import AudioRecorder from "./components/HeaderComponents/AudioRecorder.vue";
+import HeaderTitle from "./components/HeaderComponents/HeaderTitle.vue";
+import AudioRecorder from "./components/BodyComponents/AudioRecorder.vue";
 import DeviceSelector from "./components/HeaderComponents/Settings/DeviceSelector.vue";
 import SettingsIcon from "./components/HeaderComponents/Settings/SettingsIcon.vue";
 import { settingsStore } from "./stores/settings.js";
