f6061378de978b40de53f2220b1f666b6b3c2bcb
Ryan Chen <ryanchen979@gmail.com>
2024-12-02 22:15:11 -0500

    kms

--- app/src/App.vue
+++ app/src/App.vue
@@ -19,6 +19,7 @@
     </div>
 
     <TunerSection />
+    <MetronomeComponent />
     <RouterView />
   </div>
 </template>
@@ -28,11 +29,11 @@ import HeaderTitle from "./components/HeaderComponents/HeaderTitle.vue";
 import AudioRecorder from "./components/BodyComponents/AudioRecorder.vue";
 import DeviceSelector from "./components/HeaderComponents/Settings/DeviceSelector.vue";
 import SettingsIcon from "./components/HeaderComponents/Settings/SettingsIcon.vue";
+import TunerSection from "./components/TunerComponents/TunerSection.vue";
+import MetronomeComponent from "./components/MetronomeComponent.vue";
 import { settingsStore } from "./stores/settings.js";
 
 const settings = settingsStore();
-
-import TunerSection from "./components/TunerComponents/TunerSection.vue";
 </script>
 
 <style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,4 @@
-<script setup>
-import AudioRecorder from "../components/AudioRecorder.vue";
-import ToneJs from "@/components/ToneJs.vue";
-</script>
+<script setup></script>
 
 <template>
   <main></main>
