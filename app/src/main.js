17bb7c7875d104059725690e73d5063d4a1952e0
LeoDwi <leod13579@gmail.com>
2024-12-10 11:01:58 -0500

    import whoops

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,7 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import piniaPersistedState from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
 
