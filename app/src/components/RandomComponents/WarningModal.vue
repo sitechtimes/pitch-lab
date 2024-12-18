6b9da4e4e2b6fc7588ca167f89e7eec6719a2c17
LeoDwi <leod13579@gmail.com>
2024-12-18 11:02:05 -0500

    baseline first

---
+++ app/src/components/RandomComponents/WarningModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Warning!</h2>
+    <p>Selecting delete will cause these files to be lost forever.</p>
+    <p>Do you want to proceed?</p>
+    <button>Cancel</button>
+    <button>Yes</button>
+  </div>
+</template>
+
+<script setup>
+import { settingsStore } from "@/stores/settings";
+const store = settingsStore();
+</script>
