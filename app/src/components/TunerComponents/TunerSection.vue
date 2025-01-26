baf40f41f986f30f5ed682d7d8a7d79a6b08d7a6
Verviam <gaoevan37@gmail.com>
2025-01-26 13:02:33 -0500

    normalize frequency

--- app/src/components/TunerComponents/TunerSection.vue
+++ app/src/components/TunerComponents/TunerSection.vue
@@ -247,13 +247,8 @@ const detectPitch = () => {
 };
 
 const normalizeFrequency = (frequency) => {
-  while (frequency < 261.63) {
-    frequency *= 2;
-  }
-  while (frequency >= 523.25) {
-    frequency /= 2;
-  }
-  return frequency;
+  const A4 = 440;
+  return A4 * Math.pow(2, Math.log2(frequency / A4) % 1);
 };
 
 onMounted(() => {
