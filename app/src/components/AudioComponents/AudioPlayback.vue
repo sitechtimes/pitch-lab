610a3c010ee67b6ae997483ae8793d0a9ca6bd9c
LeoDwi <leod13579@gmail.com>
2025-02-03 10:51:14 -0500

    yippee

--- app/src/components/AudioComponents/AudioPlayback.vue
+++ app/src/components/AudioComponents/AudioPlayback.vue
@@ -19,33 +19,8 @@
 
 <script setup>
 import url from "../../../public/download-button.png";
-import { onMounted } from "vue";
-import { settingsStore } from "@/stores/settings.js"; // Adjust to your store path
 import { audioFiles } from "@/stores/audioFiles";
-const settings = settingsStore();
 const audioStore = audioFiles();
-
-// Methods
-const setupAudioRouting = () => {
-  const audioContext = settings.audioContext; // Get Tone.js context from the store
-
-  // Create a source node from the audio element
-  const audioSource = audioContext.createMediaElementSource(
-    audioStore.currentAudio.audio,
-  );
-
-  // Example: Apply a gain node (volume control)
-  const gainNode = audioContext.createGain();
-  gainNode.gain.value = settings.volume; // Set the initial volume from the store
-
-  // Connect the source to the gain node, and then to the Tone.js context destination (the speakers)
-  audioSource.connect(gainNode);
-  gainNode.connect(audioContext.destination);
-};
-
-onMounted(() => {
-  setupAudioRouting();
-});
 </script>
 
 <style scoped>
