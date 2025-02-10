02d64cb9ac8fe9199a85e0ca91c6f2cd8b1bcf6e
Drunk <chenk5104@gmail.com>
2025-02-10 11:02:39 -0500

    dfsdf

--- app/src/components/AudioComponents/AudioPlayback.vue
+++ app/src/components/AudioComponents/AudioPlayback.vue
@@ -20,18 +20,58 @@
         Download
       </a>
     </div>
+
+    <!-- <div>
+      <button @click="toggleWaveform">Toggle Waveform</button>
+    </div>
+
+    <div v-if="showWaveform">
+      <canvas ref="waveformCanvas"></canvas>
+    </div> -->
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import { audioFiles } from "@/stores/audioFiles";
-const audioStore = audioFiles();
+// import WaveSurfer from 'wavesurfer.js';
+
+// const audioStore = audioFiles();
+// const showWaveform = ref(false);
+// const waveformCanvas = ref(null);
+// const audioElement = ref(null);
+
+// const toggleWaveform = () => {
+//   showWaveform.value = !showWaveform.value;
+//   if (showWaveform.value) {
+//     createWaveform();
+//   }
+// };
+
+// const createWaveform = () => {
+//   const wavesurfer = WaveSurfer.create({
+//     container: waveformCanvas.value,
+//     waveColor: 'violet',
+//     progressColor: 'purple'
+//   });
+//   wavesurfer.load(audioElement.value);
+// };
+
+// onMounted(() => {
+//   if (showWaveform.value) {
+//     createWaveform();
+//   }
+// });
 </script>
 
 <style scoped>
-audio {
+/* audio {
   display: block;
   width: 100%;
   margin-bottom: 10px;
 }
+canvas {
+  width: 100%;
+  height: 100px;
+} */
 </style>
