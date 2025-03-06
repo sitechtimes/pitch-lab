397ea6e1fa384d46abfb1e8dc5bcd57c8876d88e
Verviam <gaoevan37@gmail.com>
2025-03-05 19:34:06 -0500

    sitemaps

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import TunerView from "../views/TunerView.vue";
+import LandingView from "@/views/LandingView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/tuner",
       name: "tuner",
       component: TunerView,
+    },
+    {
+      path: "/landing",
+      name: "landing",
+      component: LandingView,
     },
   ],
 });
