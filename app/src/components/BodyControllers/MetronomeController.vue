ef16d65c9fba1d1547e61f2f54eae816b8910c5a
Verviam <gaoevan37@gmail.com>
2025-03-07 11:51:40 -0500

    Update MetronomeController.vue

--- app/src/components/BodyControllers/MetronomeController.vue
+++ app/src/components/BodyControllers/MetronomeController.vue
@@ -53,13 +53,15 @@ const availableSounds = ref(["quack", "tack"]); // Add more as needed
 
 const loadSound = () => {
   isLoading.value = true;
-const soundFile =
-  selectedSound.value === "quack"
-    ? "quack.mp3"
-    : `${selectedSound.value}.mp3`;
-audio: new Audio(`${soundFile}`),
-audio.setSinkId(persistedStore.selectedSpeaker);
-audio.volume = persistedStore.outputVolume;
+
+  const soundFile =
+    selectedSound.value === "quack"
+      ? "quack.mp3"
+      : `${selectedSound.value}.mp3`;
+
+  audio = new Audio(`/${soundFile}`);
+  audio.setSinkId(persistedStore.selectedSpeaker);
+  audio.volume = persistedStore.outputVolume;
   audio.load();
   isLoading.value = false;
 };
