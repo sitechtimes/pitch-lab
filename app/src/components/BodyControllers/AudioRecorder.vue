0d2632900d5bc1054a00934883f108dd291edfc8
Kevin Chen <chenk5104@gmail.com>
2025-07-17 08:28:00 -0400

    color change

--- app/src/components/BodyControllers/AudioRecorder.vue
+++ app/src/components/BodyControllers/AudioRecorder.vue
@@ -45,13 +45,12 @@
           class="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"
         ></span>
         <span
-          class="absolute w-36 h-32 -top-8 -left-2 bg-darl-blue rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"
+          class="absolute w-36 h-32 -top-8 -left-2 bg-dark-purple rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"
         ></span>
         <span
-          class="absolute w-36 h-32 -top-8 -left-2 bg-green rotate-12 transform scale-x-0 group-hover:scale-x-50 transition-transform group-hover:duration-1000 duration-500 origin-left"
+          class="absolute w-36 h-32 -top-8 -left-2 bg-blue rotate-12 transform scale-x-0 group-hover:scale-x-50 transition-transform group-hover:duration-1000 duration-500 origin-left"
         ></span>
-        <span
-          class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10"
+        <span class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10"
           >View Recordings</span
         >
       </button>
